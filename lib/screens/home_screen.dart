86962603bd0e402eb7fb58c30a9853292013b29f
AdrissonV <adrissonvinicius02@gmail.com>
2021-10-01 21:11:34 -0300

    Foi criado a HomePage e definido o tema do app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,86 @@
 import 'package:flutter/material.dart';
 
-
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold()
-  }
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController = PageController(initialPage: 0);
+  int _page = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: 0);
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      /* appBar: AppBar(
+        title: const Text("MIssões"),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+            ),
+          ),
+        ],
+      ), */
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        backgroundColor: Colors.indigo,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white54,
+        onTap: (p) {
+          _pageController.animateToPage(p,
+              duration: Duration(milliseconds: 500), curve: Curves.ease);
+        },
+        items: [
+          BottomNavigationBarItem(
+            label: 'Missão',
+            icon: Icon(
+              Icons.emoji_flags_outlined,
+            ),
+          ),
+          BottomNavigationBarItem(
+            label: 'Chat',
+            icon: Icon(Icons.chat_bubble_outline),
+          ),
+          BottomNavigationBarItem(
+            label: 'Conta',
+            icon: Icon(Icons.account_circle_outlined),
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: [
+          Container(
+            color: Colors.amber,
+          ),
+          Container(
+            color: Colors.black,
+          ),
+          Container(
+            color: Colors.cyan,
+          ),
+        ],
+      ),
+    );
+  }
+}
